f390b9eb8fc73fddf72fc83cb908e221b68526e1
jmmill13 <jmmill13@ncsu.edu>
2016-11-10 09:51:17 -0500

    commit project 2 script

---
+++ jmmill13project2.R
@@ -0,0 +1,19 @@
+### Jessica Miller ###
+### Nov. 10, 2016 ###
+### ST 758 Proj. 2 ###
+
+#setwd("F:/ST 758/jmmill13ST758")
+
+loc1024 <- read.csv("locations1024.csv", header=T)
+loc2025 <- read.csv("locations2025.csv", header=T)
+loc4096 <- read.csv("locations4096.csv", header=T)
+
+## Plot locations on grid
+
+cov1 <- function(locations) {
+  
+}
+
+cov2 <- function(locations) {
+  
+}
